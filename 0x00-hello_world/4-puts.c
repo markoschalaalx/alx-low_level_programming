91aef648614972abf0b23ddfe53e50c4892f1b67
markoschalaalx <markchala12@gmail.com>
2022-12-08 10:37:37 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *second program in c
+ */
+/**
+ *main - entry point
+ *print a string followed by a new line using puts
+ *Return: 0
+ */
+int main(void) 
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0); 
+}
