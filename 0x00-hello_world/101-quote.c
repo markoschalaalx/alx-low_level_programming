5960fba9ec63adc2df5b194ee6c87ea09c3baeca
markoschalaalx <markchala12@gmail.com>
2022-12-09 10:32:29 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";  
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fwrite(str, sizeof(str), 1, stderr);
 return (1);
 }
+
