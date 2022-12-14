8a84e2247591a7ba3c23c69c3fcdc6fbf06e3dd8
markoschalaalx <markchala12@gmail.com>
2022-12-14 10:51:30 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
  */
-void print_alphabet(void) 
+int main(void) 
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-_putchar('\n'); 
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0); 
 }
