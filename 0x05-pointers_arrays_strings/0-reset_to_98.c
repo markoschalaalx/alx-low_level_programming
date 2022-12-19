e02ff1720bca401a5ca7ed26ba9a7cb63ad22077
markoschalaalx <markchala12@gmail.com>
2022-12-19 11:36:10 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to 98
- * @n: input
- * Return: n
+ * @n: parameter to take the value.
+ * 
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd_h>
-
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
