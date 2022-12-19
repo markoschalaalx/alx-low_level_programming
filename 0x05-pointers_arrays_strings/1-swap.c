06304ae5f55e3523bbbee2decc0c5e7868febb60
markoschalaalx <markchala12@gmail.com>
2022-12-19 11:20:21 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+/**
+ *  * swap_int - swaps the values of two integers
+ *   * @a: integer 1
+ *    * @b: integer 2
+ *     *
+ *      **/
+void swap_int(int *a, int *b)
+{
+		int c = *a;
+
+			*a = *b;
+				*b = c;
+}
+
