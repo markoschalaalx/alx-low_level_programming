10a3fb4f45e168c026f31920bc0a5a03d445560c
markoschalaalx <markchala12@gmail.com>
2022-12-20 12:22:00 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
- * main - Generates random valid passwords for the program 101-crackme.
+ * checksum - executes checksum
+ * @s: input char
+ * Return: checksum
+ */
+unsigned long checksum(char *s)
+{
+unsigned long sum = 0;
+while (*s != 0)
+{
+sum += *s;
+s++;
+}
+return (sum);
+}
+/**
+ * main - prints password for crakme
  * Return: Always 0.
  */
 int main(void)
 {
-char password[84];
-int index = 0, sum = 0, diff_half1, diff_half2;
-srand(time(0));
-while (sum < 2772)
+char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTUVWXYZ";
+char s[33];
+unsigned long sum;
+int i, flag = 0;
+srand(time(NULL));
+while (flag == 0)
 {
-password[index] = 33 + rand() % 94;
-sum += password[index++];
-}
-password[index] = '\0';
-if (sum != 2772)
+for (i = 0; i < 33; i++)
 {
-diff_half1 = (sum - 2772) / 2;
-diff_half2 = (sum - 2772) / 2;
-if ((sum - 2772) % 2 != 0)
-diff_half1++;
-for (index = 0; password[index]; index++)
+s[i] = alpha[rand() % (sizeof(alpha) - 1)];
+}
+s[i] = '\0';
+sum = checksum(s);
+if (sum == 2772)
 {
-if (password[index] >= (33 + diff_half1))
-{
-password[index] -= diff_half1;
-break;
+flag = 1;
+printf("%s", s);
 }
-}																				for (index = 0; password[index]; index++)
-}
-{
-if (password[index] >= (33 + diff_half2))
-{
-password[index] -= diff_half2;
-break;
-}
-}
-}
-printf("%s", password);
+}	
 return (0);
 }
