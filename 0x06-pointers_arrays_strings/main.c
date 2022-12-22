4c56c2a2d66da428796f508e5438de3708975846
markoschalaalx <markchala12@gmail.com>
2022-12-22 11:09:52 -0800

    main.c

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -7,7 +7,7 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
- /*
+/*
  * write your line of code here...
  * Remember:
  * - you are not allowed to use a
@@ -15,8 +15,8 @@ p = &n;
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
-*(p + 5);
- /* ...so that this prints 98\n */
+;
+/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
