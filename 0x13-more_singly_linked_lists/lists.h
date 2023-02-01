bd1e03bae06060ef5e3a630b3ac855a7d4a06e9f
markoschalaalx <markchala12@gmail.com>
2023-02-01 11:41:30 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,19 +1,26 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+   and types written in the 0x12-more_singly_linked_lists directory.
+ */
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  * Description: singly linked list node structure
- * for Holberton project
+ * for markos project
  */
 typedef struct listint_s
 {
 int n;
 struct listint_s *next;
 } listint_t;
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -29,6 +36,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-void _printd(int n);
-#endif
+
+#endif /* LISTS_H */
 
